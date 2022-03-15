fbe13c34731bb8d1d54ef3ecc06381804571c8f2
emmabraboke <you@example.com>
2022-03-15 23:26:05 +0000

    added new files

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - add the int values.
+ * @a: int b.
+ * @b: int a.
+ * Return: return sum
+ */
+int add(int a, int b)
+{
+	int sum;
+	 sum = a + b;
+	return (sum);
+}
