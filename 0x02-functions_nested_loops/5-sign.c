c6f915514091969479e31ce8ada59db7dce3d115
emmabraboke <you@example.com>
2022-03-15 17:58:26 +0000

    initial commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - checks number.
+ * @n: number.
+ * Return: 1 n is positive
+ */
+int print_sign(int n)
+{
+	if (n >= 1)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
